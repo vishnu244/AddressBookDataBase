e74be59147db97c007e7b7d75b15db50fa89751c
vishnu244 <vishnunali244@gmail.com>
2022-06-06 18:57:41 +0530

    [Vishnu]-Created AddressBook Table with Required attributes

--- Address Book service.sql
+++ Address Book service.sql
@@ -1,2 +1,16 @@
 create database AddressBookService
 use AddressBookService
+
+--------uc2-Create Address Book Table----------
+
+create table AddressBook (
+ID int Primary Key Identity (1,1),
+FirstName Varchar(100),
+LastName Varchar(100),
+Address Varchar(100),
+City Varchar(100),
+State Varchar(100),
+Zip int,
+PhoneNumber Varchar(12),
+Email_ID Varchar(100)
+);
