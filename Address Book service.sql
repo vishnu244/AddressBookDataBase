f0624f37625675d7ad42eeeba7d5dff65bf4a0a9
vishnu244 <vishnunali244@gmail.com>
2022-06-06 19:26:53 +0530

    [Vishnu]-Understanding the size of AddressBook by State or City Name

--- Address Book service.sql
+++ Address Book service.sql
@@ -39,3 +39,9 @@ Delete From AddressBook where FirstName = 'abc';
 Select * from AddressBook where City ='Temple Alwal' or State ='Telangana';
 Select * from AddressBook where City ='Vijayawada';
 Select * from AddressBook where State ='Kerala';
+
+
+-------UC7 - size of AddressBook using city or state----------
+
+select count(City) from AddressBook;
+select count(State) from AddressBook;
